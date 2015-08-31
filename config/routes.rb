6bff49a00eb7bfd40fd9476842ea0c31d69e74fc
Andreas Haselsteiner <a.haselsteiner@gmx.net>
2015-08-31 17:58:33 +0000

    implement header and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'users#index'
+  #get 'static_pages/home'
+
+  root 'static_pages#home'
+  
+  
   resources :projects
   resources :users
   resources :groups
