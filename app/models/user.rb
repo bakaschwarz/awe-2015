2e81f2c3278dd8658f717cfb2ded7d95ea4013aa
Yannick Bülter <yannick.buelter@yabue.de>
2015-09-02 19:16:03 +0200

    - User generieren nun ein API Token (thx @ Gruppe 07) - TODO: Accept Post Requests für dieses Token + JSON

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_save :generate_api_key
+
+  private
+    def generate_api_key
+      self.api_token = SecureRandom.hex
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902111355) do
+ActiveRecord::Schema.define(version: 20150902170529) do
 
   create_table "sensor_data", force: :cascade do |t|
     t.integer  "node"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150902111355) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "api_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
