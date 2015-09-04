fce2f91bf180d70b0e17bba2c6b58664ae9fcdde
Yannick Bülter <yannick.buelter@yabue.de>
2015-09-04 16:54:39 +0200

    - Helper for station sorting

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,13 @@
 module StaticPagesHelper
+
+  def ordered_timestamps_for_stations
+    sensor_data_unsorted = SensorDatum.order("sensor_data.time_stamp DESC").all
+    ordered_stamps = {}
+    sensor_data_unsorted.each do |sensor_data|
+      station = Station.find(Sensor.find(sensor_data[:sensor_id]).station_id).id
+      ordered_stamps[station] ||= {}
+      ordered_stamps[station][sensor_data[:time_stamp]] ||= {}
+      ordered_stamps[station][sensor_data[:time_stamp]][:sensor_id] = sensor_data[:value]
+    end
+  end
 end
