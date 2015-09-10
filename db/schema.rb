f08ee985abdaedd69140dbf17488fdc17977ee1a
Yannick Bülter <yannick.buelter@yabue.de>
2015-09-10 20:17:52 +0200

    shitty schema.rb weg

---
+++ db/schema.rb
@@ -0,0 +1,96 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150910113755) do
+
+  create_table "api_tokens", force: :cascade do |t|
+    t.string   "token",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "configs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "filter_range"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "main_diagrams", force: :cascade do |t|
+    t.string   "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sensor_data", force: :cascade do |t|
+    t.integer  "time_stamp", limit: 8, null: false
+    t.integer  "sensor_id",            null: false
+    t.float    "value",                null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "sensors", force: :cascade do |t|
+    t.string   "sensor"
+    t.string   "label"
+    t.string   "unit"
+    t.string   "abbreviation",                         null: false
+    t.integer  "station_id",                           null: false
+    t.integer  "main_diagram_id"
+    t.integer  "visualization_type_id"
+    t.boolean  "active",                default: true
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.text     "description"
+    t.string   "label"
+    t.integer  "node_number"
+    t.integer  "dashboard_position", default: 999
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.integer  "filter_range"
+    t.integer  "filter_precision"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "visualization_types", force: :cascade do |t|
+    t.string   "name"
+    t.string   "chart_keyword"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+end
