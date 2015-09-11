3a54cf8e877e0fc4085172006c9c862bb5222eb0
Yannick Bülter <yannick.buelter@yabue.de>
2015-09-11 11:45:58 +0200

    cucumber und so zusammen mit master und andreas aenderungen <3

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -3,10 +3,8 @@ class Sensor < ActiveRecord::Base
   belongs_to :visualization_type
   belongs_to :station
   belongs_to :main_diagram
-  before_save :set_defaults
-
-  private
-    def set_defaults
-      self.visualization_type_id ||= 1
-    end
+  validates :sensor,presence: true, allow_blank: false
+  validates :label,presence: true, allow_blank: false
+  validates :abbreviation, presence: true, allow_blank: false
+  validates :unit, presence: true, allow_blank: false
 end
